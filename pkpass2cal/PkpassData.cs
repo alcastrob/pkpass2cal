cdaa81bd244c38ed437c9b3c9c4c1531e704f1a9
Angel L. Castro <al.castro@gmail.com>
2017-12-17 21:20:55 +0100

    pkpass file management.

--- pkpass2cal/PkpassData.cs
+++ pkpass2cal/PkpassData.cs
@@ -1,62 +1,29 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pkpass2cal
 {
     public class Barcode
     {
-        public string message { get; set; }
-        public string format { get; set; }
-        public string messageEncoding { get; set; }
-        public string altText { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("format")]
+        public string Format { get; set; }
+        [JsonProperty("messageEncoding")]
+        public string MessageEncoding { get; set; }
+        [JsonProperty("altText")]
+        public string AltText { get; set; }
     }
 
     public class Location
     {
-        public double latitude { get; set; }
-        public double longitude { get; set; }
-        public string relevantText { get; set; }
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+        [JsonProperty("relevantText")]
+        public string RelevantText { get; set; }
     }
-
-    //public class HeaderField
-    //{
-    //    public string key { get; set; }
-    //    public string label { get; set; }
-    //    public string value { get; set; }
-    //}
-
-    //public class PrimaryField
-    //{
-    //    public string key { get; set; }
-    //    public string label { get; set; }
-    //    public string value { get; set; }
-    //    public string changeMessage { get; set; }
-    //}
-
-    //public class SecondaryField
-    //{
-    //    public string key { get; set; }
-    //    public string label { get; set; }
-    //    public string value { get; set; }
-    //}
-
-    //public class AuxiliaryField
-    //{
-    //    public string key { get; set; }
-    //    public string label { get; set; }
-    //    public string value { get; set; }
-    //}
-
-    //public class BackField
-    //{
-    //    public string key { get; set; }
-    //    public string label { get; set; }
-    //    public string value { get; set; }
-    //}
 
     public class Field
     {
@@ -70,30 +37,67 @@ namespace pkpass2cal
 
     public class BoardingPass
     {
-        public List<Field> headerFields { get; set; }
-        public List<Field> primaryFields { get; set; }
-        public List<Field> secondaryFields { get; set; }
-        public List<Field> auxiliaryFields { get; set; }
-        public List<Field> backFields { get; set; }
-        public string transitType { get; set; }
+        [JsonProperty("headerFields")]
+        public List<Field> HeaderFields { get; set; }
+        [JsonProperty("primaryFields")]
+        public List<Field> PrimaryFields { get; set; }
+        [JsonProperty("secondaryFields")]
+        public List<Field> SecondaryFields { get; set; }
+        [JsonProperty("auxiliaryFields")]
+        public List<Field> AuxiliaryFields { get; set; }
+        [JsonProperty("backFields")]
+        public List<Field> BackFields { get; set; }
+        [JsonProperty("transitType")]
+        public string TransitType { get; set; }
     } 
+
+    public class EventTicket
+    {
+        [JsonProperty("primaryFields")]
+        public List<Field> PrimaryFields { get; set; }
+        [JsonProperty("secondaryFields")]
+        public List<Field> SecondaryFields { get; set; }
+        [JsonProperty("auxiliaryFields")]
+        public List<Field> AuxiliaryFields { get; set; }
+        [JsonProperty("backFields")]
+        public List<Field> BackFields { get; set; }
+    }
 
     public class PkpassData
     {
-        public int formatVersion { get; set; }
-        public string passTypeIdentifier { get; set; }
-        public string serialNumber { get; set; }
-        public string teamIdentifier { get; set; }
-        public string webServiceURL { get; set; }
-        public string authenticationToken { get; set; }
-        public string organizationName { get; set; }
-        public string description { get; set; }
-        public string labelColor { get; set; }
-        public string foregroundColor { get; set; }
-        public string backgroundColor { get; set; }
-        public Barcode barcode { get; set; }
-        public BoardingPass boardingPass { get; set; }
-        public List<Location> locations { get; set; }
-        public string relevantDate { get; set; }
+        [JsonProperty("formatVersion")]
+        public int FormatVersion { get; set; }
+        [JsonProperty("passTypeIdentifier")]
+        public string PassTypeIdentifier { get; set; }
+        [JsonProperty("serialNumber")]
+        public string SerialNumber { get; set; }
+        [JsonProperty("teamIdentifier")]
+        public string TeamIdentifier { get; set; }
+        [JsonProperty("webServiceURL")]
+        public string WebServiceURL { get; set; }
+        [JsonProperty("authenticationToken")]
+        public string AuthenticationToken { get; set; }
+        [JsonProperty("organizationName")]
+        public string OrganizationName { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("labelColor")]
+        public string LabelColor { get; set; }
+        [JsonProperty("logoText")]
+        public string LogoText { get; set; }
+        [JsonProperty("foregroundColor")]
+        public string ForegroundColor { get; set; }
+        [JsonProperty("backgroundColor")]
+        public string BackgroundColor { get; set; }
+        [JsonProperty("barcode")]
+        public Barcode Barcode { get; set; }
+        [JsonProperty("boardingPass")]
+        public BoardingPass BoardingPass { get; set; }
+        [JsonProperty("locations")]
+        public List<Location> Locations { get; set; }
+        [JsonProperty("relevantDate")]
+        public string RelevantDate { get; set; }
+        [JsonProperty("eventTicket")]
+        public EventTicket EventTicket { get; set; }
     }
 }
---
+++ pkpass2cal/PkpassProcessors/KinepolisProcessor.cs
@@ -0,0 +1,70 @@
+﻿using pkpass2cal.CloudServices;
+using pkpass2cal.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pkpass2cal.PkpassProcessors
+{
+    public class KinepolisProcessor : IPkpassProcessor
+    {
+        ICloudStorageHelper cloudStorageHelper;
+
+        public KinepolisProcessor()
+        {
+            cloudStorageHelper = CloudServiceFactory.CreateCloudService();
+        }
+
+        public virtual PkpassData DownloadData(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PkpassData GetData(string filePath)
+        {
+            Path.GetFileName(filePath);
+            string destinationFileName = cloudStorageHelper.GetHomePath() + Config.CloudService.LocalDirectory + Path.GetFileName(filePath);
+            
+            //Only if they are in different paths we do the copy
+            if (destinationFileName != filePath && !File.Exists(destinationFileName))
+                File.Copy(filePath, destinationFileName);
+
+            return PkpassManager.OpenPkpass(destinationFileName);
+        }
+
+        public virtual void Process(PkpassData data, Uri uri)
+        {            
+            var parsedData = ParseData(data);
+            GCalManager.CreateAppoiment(parsedData.Item1, parsedData.Item2, parsedData.Item3, parsedData.Item4, uri.AbsoluteUri);
+        }
+
+        public virtual void Process(PkpassData data)
+        {
+            var parsedData = ParseData(data);
+            GCalManager.CreateAppoiment(parsedData.Item1, parsedData.Item2, parsedData.Item3, parsedData.Item4, string.Empty);
+        }
+
+        private (string, DateTime, DateTime, string) ParseData(PkpassData data)
+        {
+            string title = data.eventTicket.primaryFields.First(t => t.Key == "EVENTNAME").Value;
+            DateTime startTime = DateTime.Parse(data.relevantDate);
+            DateTime endTime = startTime.AddHours(2.5); //By default
+            var location = $"{data.organizationName} {data.logoText}";
+
+            return (title, startTime, endTime, location);
+        }
+
+        protected DateTime ConvertDateTime(string date, string time)
+        {
+            string[] dateParts = date.Split(new char[] { '/' });
+            string[] timeParts = time.Split(new char[] { ':' });
+
+            DateTime returnedValue = new DateTime(Convert.ToInt32(dateParts[2]), Convert.ToInt32(dateParts[1]), Convert.ToInt32(dateParts[0]),
+                Convert.ToInt32(timeParts[0]), Convert.ToInt32(timeParts[1]), 0);
+            return returnedValue;
+        }       
+    }
+}
